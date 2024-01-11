4cb9812ee3efb0de7689665ffc4676b3a2a4134e
Rodrigo Linhares <rodrigolinhares@gmail.com>
2024-01-11 12:10:46 -0300

    Assets for enemies. Renamed FollowingEnemy to Knight. Added sprite and animations for Knight.

--- Assets/Scripts/EnemyFollowMovement.cs
+++ Assets/Scripts/EnemyFollowMovement.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class EnemyFollowMovement : EnemyMovement
 {
+    private SpriteRenderer sprite;
+
+    new private void Awake()
+    {
+        base.Awake();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
     private void Update()
     {
         if (enabled)
+        {
             body.velocity = (playerBody.position - body.position).normalized * movementSpeed;
+            sprite.flipX = body.velocity.x < 0;
+        }
     }
 }
--- Assets/Scripts/EnemyZoningMovement.cs
+++ Assets/Scripts/EnemyZoningMovement.cs
@@ -6,13 +6,26 @@ using UnityEngine;
 
 public class EnemyZoningMovement : EnemyMovement
 {
+    private Animator animator;
     private float zoningDistance = 5f;
+
+    new private void Awake()
+    {
+        base.Awake();
+        animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         if (enabled && Vector2.Distance(body.position, playerBody.position) < zoningDistance)
+        {
             body.velocity = (body.position - playerBody.position).normalized * movementSpeed;
+            animator.SetBool("moving", true);
+        }
         else
+        {
             body.velocity = Vector2.zero;
+            animator.SetBool("moving", false);
+        }
     }
 }
